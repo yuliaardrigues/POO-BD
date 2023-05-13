cb6b555bcc5008f11684415c5c2a35eeeb806a5a
livia araujo <liviaaraujo.dev@gmail.com>
2023-05-13 16:51:05 -0300

    criando organizacao de pastas

---
+++ backend/config/Conexão.php
@@ -0,0 +1,13 @@
+<?php
+
+class Conexao {
+    private static $instance;
+
+    public static function getConn(){
+
+        if (!isset(self::$instance)){
+            self::$instance = new \PDO('mysql:host=localhost;dbname=pdo', 'root', '');
+        }
+        return self::$instance;
+    }
+}
